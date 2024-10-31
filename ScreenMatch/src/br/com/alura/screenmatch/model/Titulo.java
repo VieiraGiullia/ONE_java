62d20445b1a39129b9eb73965173b0d2749071ee
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-10-31 20:33:09 +0000

    fix: setters e visibilidade nos pacotes

---
+++ ScreenMatch/src/br/com/alura/screenmatch/model/Titulo.java
@@ -0,0 +1,63 @@
+package model;
+
+public class Titulo {
+     // atributos da classe
+     private String nome;
+     private int anoDeLancamento;
+     private boolean incluidoPlano;
+     private double avaliacao;
+     private double totalDeAvaliacoes;
+     private int duracaoMinutos;
+    
+    // construtor
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean incluidoPlano() {
+        return incluidoPlano;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public double getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome; // “this” se refere ao objeto atual e não ao parâmetro do método
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoPlano(boolean incluidoPlano) {
+        this.incluidoPlano = incluidoPlano;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+     
+    // metódo
+    public void exibeFichaTecnica() {
+         System.out.println("Nome do filme: " + nome);
+         System.out.println("Ano de lançamento: " + anoDeLancamento);
+     }
+ 
+     public void avalia(double nota){
+         avaliacao += nota;
+         totalDeAvaliacoes++;
+     }
+ 
+     public double mediaDasAvaliacoes(){
+         return avaliacao / totalDeAvaliacoes;
+     }
+}
